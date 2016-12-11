fc789ec51c5d9f1a5fbeac084a017dd57374fc3e
Andres Puulinn <andres.puulinn@gmail.com>
2016-12-11 23:40:52 +0200

    move sql init script into folder for mounting into docker image

--- _init/01-extensions.sql
+++ _init/01-extensions.sql
@@ -1,4 +1,4 @@
-# for full text search we need to prepare 'estonian' locale, since english locale uses stop words like 'as'
+-- for full text search we need to prepare 'estonian' locale, since english locale uses stop words like 'as'
 create text search dictionary estonian (template = pg_catalog.simple);
 create text search configuration estonian (copy = english);
 alter text search configuration estonian alter mapping for asciihword,asciiword,hword,hword_asciipart,hword_part,word with simple;
---
+++ create.sh
@@ -0,0 +1,10 @@
+# create container with downloaded data
+
+# make sure there is not temp container running
+2&>/dev/null docker rm -f firmad-temp
+
+# crete a temp container with postgres
+docker run --name firmad-temp -d -e "PGDATA=/home" --volume $PWD/_init:/docker-entrypoint-initdb.d postgres:9.6
+
+# show container logs
+docker logs -f firmad-temp
